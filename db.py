5877b5cda35ffdcd3c8354b64c8534383910eef9
Claire Wang <cw696@cornell.edu>
2020-05-10 13:44:00 +0800

    Added "genre" table in DB

--- db.py
+++ db.py
@@ -6,22 +6,26 @@ association_table = db.Table("association", db.Model.metadata,
   db.Column("book_id", db.Integer, db.ForeignKey("book.id")),
   db.Column("author_id", db.Integer, db.ForeignKey("author.id")))
 
-# Our project is a "mini-online library"
-# Many-to-many relationship:A book can have multiple authors, and an author can have published multiple books
-# One-to-many relationship: A book can have multiple reviews, but a revuew will only correspond to a book
+# Our project is a "mini-online library" that contains 4 tables: book, author, genre, and review
+# Many-to-many relationship: A book can have multiple authors, and an author can have published multiple books
+# One-to-many relationship: A book can have multiple reviews, but a review will only correspond to a book
+#                           A book will only have a single genre, but a genre can correspond to multiple books
 
 class Book(db.Model):    
   __tablename__ = "book"    
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String, nullable=False)
   published_year = db.Column(db.String, nullable=False)
+  genre_id = db.Column(db.Integer, db.ForeignKey("genre.id"), nullable=False)
   authors = db.relationship("Author", secondary=association_table, back_populates="books")
   reviews = db.relationship("Review", cascade="delete")
   def serialize_book(self):
+    genre = Genre.query.filter_by(id=self.id).first()
     return {            
       "id": self.id,
       "title": self.title,
       "published_year": self.published_year,
+      "genre": {"id": genre.id, "name": genre.name},
       "authors": [x.serialize_author() for x in self.authors],
       "reviews": [y.serialize_review() for y in self.reviews]
     }
@@ -31,7 +35,20 @@ class Book(db.Model):
       "id": self.id,
       "title": self.title,
       "published_year": self.published_year,
+      "genre": {"id": genre.id, "name": genre.name},
       "reviews": [x.serialize_review() for x in self.reviews]
+    }
+
+class Genre(db.Model):
+  __tablename__ = "genre" 
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String, nullable=False)
+  books = db.relationship("Book")
+  def serialize_genre(self):
+    return {            
+      "id": self.id,
+      "name": self.name,
+      "books": [x.serialize_book_short() for x in self.books]
     }
 
 class Review(db.Model):    
