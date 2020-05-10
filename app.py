fe797316052f441afcd7ffae31278450e13aad32
Calvin-S <19calvins@tas.tw>
2020-05-10 10:29:19 -0400

    app and dao update
    began simple functions in app.py

--- app.py
+++ app.py
@@ -22,3 +22,93 @@ def failure_response(message, code=404):
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+@app.route("/api/book/<int:id>/", methods = ['GET'])
+def get_book(id):
+    book = dao.get_book(int(id))
+    if book is None:
+        return failure_response("There is no book with this id")
+    return success_response(book)
+
+@app.route("/api/book/<int:id>/", methods = ['DELETE'])
+def del_course(id):
+    book = dao.delete_book(id)
+    if course is None:
+        return failure_response("There is no book with this id")
+    return success_response(book)
+
+@app.route("/api/genre/<int:id>/", methods = ['GET'])
+def get_genre(id):
+    genre = dao.get_genre(int(id))
+    if genre is None:
+        return failure_response("There is no genre with this id")
+    return success_response(genre)
+
+@app.route("/api/author/<int:id>/", methods = ['GET'])
+def get_author(id):
+    author = dao.get_author(int(id))
+    if author is None:
+        return failure_response("There is no author with this id")
+    return success_response(author)
+
+@app.route("/api/book/", methods = ['GET'])
+def get_all_books():
+    a = dao.get_all_books()
+    if a is None:
+        return failure_response("There are no books.")
+    return success_response(a)
+
+@app.route("/api/author/", methods = ['GET'])
+def get_all_authors():
+    a = dao.get_all_authors()
+    if a is None:
+        return failure_response("There are no authors.")
+    return success_response(a)
+
+@app.route("/api/review/", methods = ['GET'])
+def get_all_reviews():
+    a = dao.get_all_reviews()
+    if a is None:
+        return failure_response("There are no reviews.")
+    return success_response(a)
+
+@app.route("/api/genre/", methods = ['GET'])
+def get_all_genre():
+    a = dao.get_all_genre()
+    if a is None:
+        return failure_response("There are no genres.")
+    return success_response(a)
+
+@app.route("/api/book/", methods = ['POST'])
+def create_book():
+    body = json.loads(request.data)
+    title = body.get("title","")
+    published_year = body.get("year","")
+    if title == "" or published_year == ""
+        return failure_response("Please provide a title and a published year")
+    return success_response(dao.add_book(title, published_year))
+
+@app.route("/api/author/", methods = ['POST'])
+def create_author():
+    body = json.loads(request.data)
+    name = body.get("name","")
+    if name = ""
+        return failure_response("Please provide a name")
+    return success_response(dao.create_author(name))
+
+@app.route("/api/review/", methods = ['POST'])
+def create_review():
+    body = json.loads(request.data)
+    review = body.get("review","")
+    book_id = body.get("id","")
+    if review == "" or book_id == ""
+        return failure_response("Please provide a review and a book id")
+    return success_response(dao.create_review(review, book_id))
+
+@app.route("/api/genre/", methods = ['POST'])
+def create_genre():
+    body = json.loads(request.data)
+    name = body.get("name","")
+    if name = ""
+        return failure_response("Please provide a name for the genre")
+    return success_response(dao.create_genre(name))
--- dao.py
+++ dao.py
@@ -1,4 +1,10 @@
-from db import db, Book, Author, Review, association_table
+from db import db, Book, Author, Review, association_table, Genre
+
+def get_book(book_id):
+  book = Book.query.filter_by(id=book_id).first()
+  if book is None:
+    return None
+  return book.serialize_book()
 
 def add_book(book_title, book_published_year):
   new_book = Book(title = book_title, published_year = book_published_year)
@@ -13,6 +19,9 @@ def delete_book(book_id):
   db.session.delete(delete_book)
   db.session.commit()
   return delete_book.serialize_book()
+
+def get_all_books():
+  return [b.serialize_book() for b in Book.query.all()]
 
 def create_author(author_name):
   new_author = Author(name = author_name)
@@ -35,11 +44,41 @@ def update_book_with_new_author(author_id, book_id):
   db.session.commit()
   return book.serialize_book()
 
+def get_all_authors():
+  return [a.serialize_author() for a in Author.query.all()]
+
 def create_review(review_text, review_book_id):
   book = Book.query.filter_by(id=review_book_id).first()
   if book is None:
     return None
-  new_reivew = Review(content = review_text, book_id = review_book_id)
-  db.session.add(new_reivew)
+  new_review = Review(content = review_text, book_id = review_book_id)
+  db.session.add(new_review)
   db.session.commit()
-  return new_reivew.serialize_review()
+  return new_review.serialize_review()
+
+def get_all_reviews():
+  return [a.serialize_review() for a in Review.query.all()]
+
+def create_genre(g_name):
+  new_genre = Genre(name=g_name)
+  db.session.add(new_genre)
+  db.session.commit()
+  return new_genre.serialize_genre()
+
+def update_book_with_genre(genre_id, book_id):
+  genre = Genre.query.filter_by(id=genre_id).first()
+  book = Book.query.filter_by(id=book_id).first()
+  if genre is None:
+    return None
+  genre.books.append(book)
+  db.session.commit()
+  return genre.serialize_genre()
+
+def get_genre(genre_id):
+  genre = Genre.query.filter_by(id=genre_id).first()
+  if genre is None:
+    return None
+  return genre.serialize_genre()
+
+def get_all_genre():
+  return [g.serialize_genre() for g in Genre.query.all()]
